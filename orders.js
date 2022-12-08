d114fc38066afafa483cee6cbf8daf46594e5055
LCS32 <lucaspontesmarques23@gmail.com>
2022-12-08 01:01:35 +0100

    finish

---
+++ orders.js
@@ -0,0 +1,45 @@
+const Orders = [
+    {
+        productName: 'Xros Mini',
+        productNumber: '56835',
+        paymentStatus: 'Due',
+        shipping:'Delivered',
+    },
+    {
+        productName: 'Xros Nano',
+        productNumber: '23678',
+        paymentStatus: 'Refonded',
+        shipping:'Pending',
+    },
+    {
+        productName: 'Xros 2',
+        productNumber: '66523',
+        paymentStatus: 'Paid',
+        shipping:'Declined',
+    },
+    {
+        productName: 'Caliburn G2',
+        productNumber: '85357',
+        paymentStatus: 'Paid',
+        shipping:'Delivered',
+    },
+    {
+        productName: 'Vinci Q',
+        productNumber: '04859',
+        paymentStatus: 'Refonded',
+        shipping:'Pending',
+    },
+    {
+        productName: 'OXVA Slim',
+        productNumber: '20845',
+        paymentStatus: 'Due',
+        shipping:'Delivered',
+    },
+    {
+        productName: 'PM 40',
+        productNumber: '12094',
+        paymentStatus: 'Paid',
+        shipping:'Declined',
+    },
+
+]
